b89add76560c3d676e219bfd50eee7e47b69f21f
lecdias <luiz.dias@perpetuo.g12.br>
2024-05-17 18:27:02 -0300

    some changes at 'kitchanpage'

--- client/src/pages/Home/KitchenSide/KitchenHome/KitchenPage.js
+++ client/src/pages/Home/KitchenSide/KitchenHome/KitchenPage.js
@@ -1,108 +1,85 @@
-import React, {useState, useEffect} from "react";
-import {useNavigate, Link} from 'react-router-dom';
-import { FaCheck as Check} from "react-icons/fa";
-import { MdOutlinePayments as Cash,  MdOutlineCleaningServices as Clean, MdHome as Home, MdTableBar as Table} from "react-icons/md";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { FaCheck as Check } from "react-icons/fa";
+import { MdOutlinePayments as Cash, MdOutlineCleaningServices as Clean, MdHome as Home, MdTableBar as Table } from "react-icons/md";
 import Cookies from 'universal-cookie';
 import api from "../../../../services/api";
-import { cookies } from "next/headers";
 import Box from '@mui/material/Box';
 
-export default function KitchenPage()
-{
+export default function KitchenPage() {
     const [pedidos, setPedidos] = useState([]);
-    const navigate = useNavigate ();
+    const [pedidoCarregado, setPedidoCarregado] = useState(false);
+    const navigate = useNavigate();
     const cookies = new Cookies();
 
     useEffect(() => {
-        const token = cookies.get('SessionId')
-        if (token)
-        {
-            getOrders(token)
-        }
-        else{
+        const token = cookies.get('SessionId');
+        if (token) {
+            getOrders(token);
+        } else {
             alert('É necessário estar logado para esta operação.');
             navigate('/login');
         }
 
-        async function getOrders(token)
-        {
-            api.post('http://localhost:8080/api/pedidos/get-all', token)
-            .then(response => {
+        async function getOrders(token) {
+            try {
+                const response = await api.post('http://localhost:8080/api/pedidos/get-all', { token });
                 console.log(response.data);
                 setPedidos(response.data);
-            })
-            .catch(error => {
+            } catch (error) {
                 alert(error.response.data.message);
                 // navigate('/home');
-            })
+            } finally {
+                setPedidoCarregado(true);
+            }
         }
-    }, [])
+    }, [navigate, cookies]);
+
+    function pedidoNull() {
+        return (
+            <div>
+                Por enquanto está tudo calmo, beba uma água, quando um pedido aparecer, um alerta sonoro soará
+            </div>
+        );
+    }
+
+    function pedidosMap() {
+        return (
+            <div>
+                {
+                    pedidos.map(pedido => (
+                        <li key={pedido.idPedido}>
+                            <Box>
+                                {pedido.dataPedido}
+                                <br />
+                                {pedido.horaPedido}
+                                <br />
+                                <ul>
+                                    {pedido.produtos.map(produto => (
+                                        <li key={produto.idProd}>
+                                            {produto.nomeProd}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Box>
+                        </li>
+                    ))
+                }
+            </div>
+        );
+    }
+
+    if (!pedidoCarregado) {
+        return (
+            <div>
+                Carregando...
+            </div>
+        );
+    }
+
     return (
         <div>
-            {
-                pedidos.length > 0 ? 
-                (
-                    pedidos.pedidoPronto === 'false' ? 
-                    (
-                        <div>
-                            <ul>
-                            {
-                                pedidos.map(pedido => 
-                                    <li key={pedido.idPedido}>
-                                        <Box>
-                                            {pedido.dataPedido}
-                                            <br></br>
-                                            {pedido.horaPedido}
-                                            <br></br>
-                                            <ul>
-                                                {pedido.produtos.map(produto => (
-                                                    <li key={produto.idProd}>
-                                                        {produto.nomeProd}
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </Box>
-                                    </li>
-                                )
-                            }
-                        </ul>
-                        </div>
-                    )
-                    :
-                    (
-                        <div>
-                            <strong>PEDIDOS ANTERIORES</strong>
-                            <ul>
-                            {
-                                pedidos.map(pedido => 
-                                    <li key={pedido.idPedido}>
-                                        <Box>
-                                            {pedido.dataPedido}
-                                            <br></br>
-                                            {pedido.horaPedido}
-                                            <br></br>
-                                            <ul>
-                                                {pedido.produtos.map(produto => (
-                                                    <li key={produto.idProd}>
-                                                        {produto.nomeProd}
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </Box>
-                                    </li>
-                                )
-                            }
-                        </ul>
-                        </div>
-                    )
-                )
-                :
-                (
-                    <div>
-                        nenhum pedido por equato
-                    </div>
-                )
-            }
+            {pedidos.length === 0 ? pedidoNull() : pedidosMap()}
         </div>
     );
 }
