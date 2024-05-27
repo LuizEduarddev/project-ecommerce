ef703cdb378f619ac1ebe24f1205f1f075901f36
LuizEduarddev <luiz.dias@perpetuo.g12.br>
2024-05-27 18:31:12 -0300

    Finalizando a página 'Orders'

--- client/src/pages/Home/AdminSide/Dashboard/Orders.js
+++ client/src/pages/Home/AdminSide/Dashboard/Orders.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Link from '@mui/material/Link';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -11,42 +11,62 @@ import { useNavigate } from 'react-router-dom';
 import api from '../../../../services/api';
 
 export default function Orders() {
-  
+
   const cookie = new Cookies();
   const navigate = useNavigate();
   const [allOrders, setAllOrders] = useState([]);
   const [loading, setLoading] = useState(true);
-
-  const session = cookie.get('SessionId');
-
-  if (session === null)
-  {
-    alert('Um erro ocorreu ao tentar pegar o usuário.');
-    navigate('/login');
-  }
-
-  useEffect(() => {
-
-    async function getData(token)
-    {
-      api.post('http://localhost:8080/api/pedidos/get-all-admin', token)
-      .then(response => {
-        const payment = response.data.pedidosList.filter(prod => prod.pedidoPago === true);
-        console.log(payment);  
-        setAllOrders(payment);
-        setLoading(false);
-      })
-      .catch(error => {
-        alert(error.response.data.message);
-        navigate('/login');
-      })
-    }
+  const [userNames, setUserNames] = useState({});
   
-    getData(session);
-  }, [])
+  
+  useEffect(() => {
+    const session = cookie.get('SessionId');
+    
+    if (session)
+    {
+      async function getData(token) {
+        try {
+          const response = await api.post('http://localhost:8080/api/pedidos/get-all-admin', token);
+          const payment = response.data.pedidosList.filter(prod => prod.pedidoPago === true);
+          setAllOrders(payment);
+          setLoading(false);
+          fetchUserNames(payment, session);
+        } catch (error) {
+          alert(error.response.data.message);
+          navigate('/login');
+        }
+      }
 
-  if (loading)
-  {
+      getData(session);
+    }
+    else{
+      alert('É necessário estar logado para realizar esta ação.');
+      navigate('/login');
+    }
+
+    async function fetchUserNames(orders, session)
+    {
+      let userNamesTemp = {}
+      orders.map(pedido => {
+        const data = {
+          id: pedido.users.idUser,
+          token: session
+        }
+        api.post('http://localhost:8080/api/auth/get-by-id', data)
+        .then(response => {
+          console.log(response);
+          userNamesTemp[pedido.users.idUser] = response.data.nome;
+        })
+        .catch(error => {
+          userNamesTemp[pedido.users.idUser] = "null";
+          console.log(error.response.data.message);
+        })
+      })
+      setUserNames(userNamesTemp);
+    }
+  }, []);
+
+  if (loading) {
     return (
       <div>
         Carregando.....
@@ -54,31 +74,12 @@ export default function Orders() {
     ); 
   }
 
-  async function getUser(idUser)
-  {
-    const data = {
-      id: idUser,
-      token: session
-    }
-    api.post('http://localhost:8080/api/auth/get-by-id', data)
-    .then(response => {
-      console.log(response);
-      return "João"
-    })
-    .catch(error => {
-      console.log(error.response.data.message);
-      return "null";
-    })
-  }
-
-  function transformMoeda(valor)
-  {
-      const formattedTotal = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-      }).format(valor);
-
-      return formattedTotal;
+  function transformMoeda(valor) {
+    const formattedTotal = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(valor);
+    return formattedTotal;
   }
 
   function preventDefault(event) {
@@ -102,8 +103,8 @@ export default function Orders() {
             <TableRow key={order.idPedido}>
               <TableCell>{order.dataPedido}</TableCell>
               <TableCell>{order.horaPedido}</TableCell>
-              <TableCell>{getUser(order.users.idUser)}</TableCell>
-              <TableCell align="right">{`${transformMoeda(order.totalPedido)}`}</TableCell>
+              <TableCell>{userNames[order.users.idUser]}</TableCell>
+              <TableCell align="right">{transformMoeda(order.totalPedido)}</TableCell>
             </TableRow>
           ))}
         </TableBody>
